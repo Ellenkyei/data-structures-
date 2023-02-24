09cbf067a4aaf9c7e4d42cd255ab7a84210da42b
Ellenkyei <125478873+Ellenkyei@users.noreply.github.com>
2023-02-24 10:45:57 +0000

    Add files via upload

---
+++ assignment4 6940221.py
@@ -0,0 +1,75 @@
+#https://github.com/Ellenkyei
+#script car pricing 
+#import numpy as np 
+carType=input('Welcome to SCENERY CAR SHOWROOM.Please enter your choice of car:')
+if carType =='zidan':
+    print('700000 ghana cedis')
+elif carType =='SUV':
+    print('3.2million dollars')
+    print('5140000')
+elif carType == 'ford':
+    print('3.5million dollars ')
+elif carType == 'mercedes':
+    print('3million dollars ')
+elif carType == 'nissan':
+    print('5million dollars ')
+elif carType == 'honda':
+    print('15million dollars ')
+elif carType == 'zonda':
+    print('6.7million dollars ')
+elif carType == 'mercedes benz':
+    print('55million dollars ')
+elif carType == 'jeep':
+    print('7million dollars ')
+elif carType == 'range over':
+    print('55million dollars ')
+elif carType == 'land cruiser':
+    print('100million dollars ')
+elif carType == 'corolla':
+    print('4million dollars ')
+elif carType == 'kia':
+    print('4.5million dollars ')
+elif carType == 'toyota':
+    print('3.5million dollars ')
+elif carType == 'hyundai':
+    print('8million dollars ')
+elif carType == 'tesla':
+    print('9million dollars ') 
+elif carType == 'luxury suv':
+    print('12million dollars ')
+elif carType == 'truck':
+    print('2.3million dollars ')
+elif carType == 'mazda':
+    print('7.8million dollars ')
+else:
+    print('sorry car is out of stock')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
